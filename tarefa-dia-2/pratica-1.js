5a1ed8146753d6f1087d3f213d157828d5b7e610
Rafael <rafaeldebner08@gmail.com>
2022-08-24 22:13:33 -0300

    Pratica 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,10 +1,17 @@
 let nome = "Rafael"
+console.log(nome)
 let email
 email = "rafaeldebner08gmail.com"
+console.log(email)
 let profissao
 profissao = "estudante"
+console.log(profissao)
 let idade
 idade = 13
+console.log(idade)
 let lugar
-lugar = "Chile" 
+lugar = "Chile"
+console.log(lugar)
+//olá
+ 
 
