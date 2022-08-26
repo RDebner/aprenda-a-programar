1b29f8b7da4dc0ad95b2c51a8ea39e18a5ca8953
Rafael <rafaeldebner08@gmail.com>
2022-08-25 21:37:30 -0300

    tabuada

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,9 @@
+let n = 5
+let mpor = 1
+while (mpor<=10){
+    let resultado = n * mpor
+    let texto = `${n} x ${mpor}`
+    console.log(texto)
+    console.log(resultado)
+    mpor = mpor + 1
+}
