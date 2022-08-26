e2f5a5e0caa8f676b81801dd7afce80d9d27fb6b
Rafael <rafaeldebner08@gmail.com>
2022-08-25 22:19:45 -0300

    tabuada

--- tarefa-dia-3/pratica-2.js
+++ tarefa-dia-3/pratica-2.js
@@ -2,8 +2,7 @@ let n = 5
 let mpor = 1
 while (mpor<=10){
     let resultado = n * mpor
-    let texto = `${n} x ${mpor}`
+    let texto = `${n} x ${mpor} = ${resultado}`
     console.log(texto)
-    console.log(resultado)
     mpor = mpor + 1
 }
