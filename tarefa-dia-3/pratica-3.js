3783d6bf7eccedde00ab59090a49141c9c341823
Rafael <rafaeldebner08@gmail.com>
2022-08-26 11:36:12 -0300

    par ou impar

---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,12 @@
+let i = 1 
+let a = 50
+ while (i<=a){
+    if (i % 2 == 0){
+        let par = `${i} é par`
+        console.log(par)
+    } else {
+        let impar = `${i} é impar`
+        console.log(impar)
+    }
+    i++
+ }
